0cf29491a4bc2ca629eae113a9c28d93900fb2c5
Kaivalya-oss <72602657+Kaivalya-oss@users.noreply.github.com>
2020-11-05 12:55:18 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,9 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+          //add the width=50, height=50
+      super(x,y,50,50);
+      this.image = loadImage("sprites/enemy.png");
+
+    }    
+  };
+  
